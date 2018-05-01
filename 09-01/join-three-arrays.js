885484620c8767def93fe507eca7ce26ab7d80d5
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-01 13:05:18 -0700

    join three arrays

---
+++ 09-01/join-three-arrays.js
@@ -0,0 +1,11 @@
+/**
+ * Given three arrays, "joinThreeArrays" 
+ * returns an array with the elements of "arr1" in order 
+ * followed by the elements in "arr2" in order 
+ * followed by the elements of "arr3" in order.
+ */
+ 
+ function joinThreeArrays(arr1, arr2, arr3) {
+  return arr1.concat(arr2, arr3);
+}
+joinThreeArrays([1, 2], [3, 4], [5, 6]);
